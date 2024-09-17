92e2bfeb0dda6b4cb3ea7cb529087e798dbec37c
Jarrell Johnson <jarrelljohnsoninfo@gmail.com>
2024-09-16 20:21:26 -0500

    Part2 Studio

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,22 @@
 package org.launchcode;
-
+import org.launchcode.Menu;
+import org.launchcode.MenuItem;
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        System.out.println("Statrup");
+       MenuItem item1 = new MenuItem(20, "cheesecake", "dessert", true);
+       MenuItem item2 = new MenuItem(40, "steak", "main course", false);
+       MenuItem item3 = new MenuItem(30, "calamari", "appetizer", false);
+
+ArrayList<MenuItem> items = new ArrayList();
+items.add(item1);
+items.add(item2);
+items.add(item3);
+        //Menu  m1 = new Menu();
+
+
     }
 }
